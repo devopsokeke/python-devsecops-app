a761f7f9d2aa937ae84573c1556a3dde8271489a
devopsokeke <devopsokeke@gmail.com>
2025-02-01 11:36:29 -0500

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Deploying the Python DevSecOps App..."
+docker stop my-app || true
+docker rm my-app || true
+docker pull my-docker-repo/my-python-app:latest
+docker run -d -p 5000:5000 --name my-app my-docker-repo/my-python-app:latest
+echo "Deployment completed!"
