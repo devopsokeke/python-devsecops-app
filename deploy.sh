fa6822668c03181d6602694992cab6e21a1bae5c
devopsokeke <devopsokeke@gmail.com>
2025-02-03 09:53:55 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ echo "Deploying the Python DevSecOps App..."
 docker stop my-app || true
 docker rm my-app || true
 docker pull my-docker-repo/my-python-app:latest
-docker run -d -p 5000:5000 --name my-app my-docker-repo/my-python-app:latest
+docker run -d -p 5001:5000 --name my-app my-docker-repo/my-python-app:latest
 echo "Deployment completed!"
